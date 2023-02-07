2ca74be2f184e53708fad7fb9df18d8e6a799b61
Yesswine <124653110+Yesswine@users.noreply.github.com>
2023-02-07 10:18:41 +0530

    Add files via upload

---
+++ Numbers.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+class Numbers{
+public static void main(String args[]){
+int n,sum=0,pro=1;
+int a[]={1,2,3,4,5,6};
+n=a.nextInt();
+int q=1;
+pro=pro*a[i];
+for(int i=0;i<=n;i++)
+{
+	if(q==1)
+	{
+	sum=sum+a[i];}
+
+System.out.println(sum);
+{
+else 
+{
+System.out.println(pro);
+}}}}}
